761321c9942e1dfb713dc015fc807412087cdaab
DE-Uehara <r.uehara@defide-ix.com>
2024-07-02 11:56:03 +0900

    認証用ページの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <Link href={"/signin"}>SignIn</Link>
       <Link href={"/signup"}>SignUp</Link>
     </div>
   );
